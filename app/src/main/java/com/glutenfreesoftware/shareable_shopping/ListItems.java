71304efa2a42c01629eadf1dd15f408f3a12cca5
Kristian <krihus05@gmail.com>
2017-11-17 13:01:58 +0100

    DeleteRooms implementert

--- app/src/main/java/com/glutenfreesoftware/shareable_shopping/ListItems.java
+++ app/src/main/java/com/glutenfreesoftware/shareable_shopping/ListItems.java
@@ -9,6 +9,7 @@ import android.app.AlertDialog;
 import android.app.Fragment;
 import android.content.DialogInterface;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.annotation.Nullable;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -19,9 +20,19 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Created by oebar on 08.11.2017.
@@ -34,12 +45,16 @@ public class ListItems extends Fragment {
     private RecyclerView.LayoutManager layoutManager;
 
     private String username = "";
-    private String room = "";
+    private String list = "";
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.layout_list_items, container, false);
+
+        username = getArguments().getString("username");
+        list = getArguments().getString("list");
+        getActivity().setTitle(list);
 
         Button deleteListBtn = (Button) view.findViewById(R.id.delete_list);
         deleteListBtn.setOnClickListener(new View.OnClickListener() {
@@ -77,10 +92,10 @@ public class ListItems extends Fragment {
         Button addItemBtn = (Button) view.findViewById(R.id.add_item);
         addItemBtn.setOnClickListener(new View.OnClickListener() {
 
-            String addedItem = "";
+            String ItemItemToAdd = "";
 
             @Override
-            public void onClick(View v) {
+            public void onClick(final View v) {
 
                 AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
                 builder.setTitle("Add item");
@@ -95,9 +110,21 @@ public class ListItems extends Fragment {
 
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
-                        addedItem = input.getText().toString();
+                        ItemItemToAdd = input.getText().toString();
 
-                        System.out.println(addedItem);
+                        addListItem(v, list, ItemItemToAdd, username);
+                        final Handler handler = new Handler();
+                        handler.postDelayed(new Runnable() {
+                            @Override
+                            public void run() {
+                                // Do something after 3s = 3000ms
+                                getListItemsMethod();
+                            }
+                        }, 3000);
+                        System.out.println("user:" + ItemItemToAdd);
+                        //addedRoom = input.getText().toString();
+
+                        System.out.println(ItemItemToAdd);
                     }
                 });
                 builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
@@ -137,6 +164,8 @@ public class ListItems extends Fragment {
                     public void onClick(DialogInterface dialog, int which) {
                         addedUser = input.getText().toString();
 
+
+
                         System.out.println("user:" + addedUser);
                     }
                 });
@@ -155,10 +184,8 @@ public class ListItems extends Fragment {
 
         });
 
-        username = getArguments().getString("username");
-        room = getArguments().getString("list");
-        getActivity().setTitle(room);
-        System.out.println("ListItem: " + username + " " + room);
+
+        System.out.println("ListItem: " + username + " " + list);
 
         recyclerView = (RecyclerView) view.findViewById(R.id.rooms_recyclerview);
         // use this setting to
@@ -169,7 +196,9 @@ public class ListItems extends Fragment {
         // use a linear layout manager
         layoutManager = new LinearLayoutManager(getActivity());
         recyclerView.setLayoutManager(layoutManager);
+        getListItemsMethod();
 
+        /*
         final List<ListItemObj> input = new ArrayList<>();
         try {
             new getListItems(new getListItems.OnPostExecute() {
@@ -189,6 +218,7 @@ public class ListItems extends Fragment {
         } catch (Exception e) {
             e.printStackTrace();
         }
+        */
         //"http://158.38.193.60:8080/Shareable-Shopping-List-REST/api/lists/getLists?listRoom=Oppskrifter&listOwner=Kristian"
         //
         //http://158.38.193.60:8080/Shareable-Shopping-List-REST/api/lists/getLists?listRoom=Oppskrifter&listOwner=Kristian
@@ -209,5 +239,62 @@ public class ListItems extends Fragment {
         super.onViewCreated(view, savedInstanceState);
 
         //getActivity().setTitle("List Items");
+    }
+
+    public void getListItemsMethod(){
+        final List<ListItemObj> input = new ArrayList<>();
+        try {
+            new getListItems(new getListItems.OnPostExecute() {
+                @Override
+                public void onPostExecute(List<ListItemObj> listItems) {
+                    if (listItems.isEmpty()) {
+                        System.out.println("listItems is empty");
+                    }
+                    for (ListItemObj l : listItems) {
+                        System.out.println(l.getListItemList() + " " + l.getListItemName() + " " + l.getListItemOwner());
+                        input.add(l);
+                    }
+                    mAdapter = new ListItemAdapter(username, input);
+                    recyclerView.setAdapter(mAdapter);
+                }
+            }).execute(new URL("http://158.38.193.60:8080/Shareable-Shopping-List-REST/api/lists/getListItems?listItemList="+list+"&listItemOwner="+username));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void addListItem(View v, String listItemList, String listItemName, String listItemOwner) {
+        RequestQueue queue = Volley.newRequestQueue(v.getContext());
+        String url = "http://158.38.193.60:8080/Shareable-Shopping-List-REST/api/lists/addListItem?listItemList=" + listItemList + "&listItemName=" + listItemName + "&listItemOwner=" + listItemOwner;
+        // Request a string response from the provided URL.
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        // Display the first 500 characters of the response string.
+                        //System.out.println("Response is: "+ response /*response.substring(0,500)*/);
+                    }
+
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                System.out.println("That didn't work!");
+            }
+
+        }) {
+            @Override
+            public String getBodyContentType() {
+                return "application/json";
+            }
+
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                Map<String, String> params = new HashMap<String, String>();
+                params.put("Content-Type", " application/json");
+                return params;
+            }
+        };
+        // Add the request to the RequestQueue.
+        queue.add(stringRequest);
     }
 }
--- app/src/main/java/com/glutenfreesoftware/shareable_shopping/ShoppingLists.java
+++ app/src/main/java/com/glutenfreesoftware/shareable_shopping/ShoppingLists.java
@@ -5,6 +5,7 @@ import android.app.AlertDialog;
 import android.app.Fragment;
 import android.content.DialogInterface;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.annotation.Nullable;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -16,9 +17,19 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Created by oebar on 08.11.2017.
@@ -38,10 +49,14 @@ public class ShoppingLists extends Fragment {
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.layout_shopping_lists, container, false);
 
+        username = getArguments().getString("username");
+        room = getArguments().getString("room");
+        getActivity().setTitle(room);
+
         Button deleteRoomBtn = (Button) view.findViewById(R.id.delete_room);
         deleteRoomBtn.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
+            public void onClick(final View v) {
 
                 AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
                 builder.setTitle("Are you sure you wish to delete room?");
@@ -51,9 +66,15 @@ public class ShoppingLists extends Fragment {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
 
-                        /*
-                        Insert code for deletion of room here
-                         */
+                        deleteRoom(v, room, username);
+                        final Handler handler = new Handler();
+                        handler.postDelayed(new Runnable() {
+                            @Override
+                            public void run() {
+                                // Do something after 3s = 3000ms
+                                getListsMethod();
+                            }
+                        }, 3000);
 
                     }
                 });
@@ -74,10 +95,10 @@ public class ShoppingLists extends Fragment {
         Button addListBtn = (Button) view.findViewById(R.id.add_list);
         addListBtn.setOnClickListener(new View.OnClickListener() {
 
-            String addedList = "";
+            String listToAdd = "";
 
             @Override
-            public void onClick(View v) {
+            public void onClick(final View v) {
 
                 AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
                 builder.setTitle("Add list");
@@ -92,9 +113,22 @@ public class ShoppingLists extends Fragment {
 
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
-                        addedList = input.getText().toString();
+                        listToAdd = input.getText().toString();
 
-                        System.out.println("user:" + addedList);
+                        addList(v, room, listToAdd, username);
+                        final Handler handler = new Handler();
+                        handler.postDelayed(new Runnable() {
+                            @Override
+                            public void run() {
+                                // Do something after 3s = 3000ms
+                                getListsMethod();
+                            }
+                        }, 3000);
+                        System.out.println("user:" + listToAdd);
+                        //addedRoom = input.getText().toString();
+
+
+                        System.out.println("user:" + listToAdd);
                     }
                 });
                 builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
@@ -153,9 +187,7 @@ public class ShoppingLists extends Fragment {
 
         });
 
-        username = getArguments().getString("username");
-        room = getArguments().getString("room");
-        getActivity().setTitle(room);
+
         System.out.println("Shopping list: " + username + " " + room);
 
         recyclerView = (RecyclerView) view.findViewById(R.id.rooms_recyclerview);
@@ -227,7 +259,7 @@ public class ShoppingLists extends Fragment {
                         System.out.println(l.getListRoom() + " " + l.getListName() + " " + l.getListOwner());
                         input.add(l);
                     }
-                    mAdapter = new ListAdapter("Kristian", input);
+                    mAdapter = new ListAdapter(username, input);
                     recyclerView.setAdapter(mAdapter);
                 }
             }).execute(new URL("http://158.38.193.60:8080/Shareable-Shopping-List-REST/api/lists/getLists?listRoom="+room+"&listOwner="+username));
@@ -235,6 +267,116 @@ public class ShoppingLists extends Fragment {
             e.printStackTrace();
         }
 
+    }
+
+    public void deleteRoom(View v, String roomName, String roomOwner) {
+        RequestQueue queue = Volley.newRequestQueue(v.getContext());
+        String url = "http://158.38.193.60:8080/Shareable-Shopping-List-REST/api/rooms/removeRoom?roomName=" + roomName + "&roomOwner=" + roomOwner;
+        // Request a string response from the provided URL.
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        // Display the first 500 characters of the response string.
+                        //System.out.println("Response is: "+ response /*response.substring(0,500)*/);
+                    }
+
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                System.out.println("That didn't work!");
+            }
+
+        }) {
+            @Override
+            public String getBodyContentType() {
+                return "application/json";
+            }
+
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                Map<String, String> params = new HashMap<String, String>();
+                params.put("Content-Type", " application/json");
+                return params;
+            }
+        };
+        // Add the request to the RequestQueue.
+        queue.add(stringRequest);
+    }
+
+    public void addList(View v, String listRoom, String listName, String listOwner) {
+        RequestQueue queue = Volley.newRequestQueue(v.getContext());
+        String url = "http://158.38.193.60:8080/Shareable-Shopping-List-REST/api/lists/addList?listRoom=" + listRoom + "&listName=" + listName + "&listOwner=" + listOwner;
+        // Request a string response from the provided URL.
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        // Display the first 500 characters of the response string.
+                        //System.out.println("Response is: "+ response /*response.substring(0,500)*/);
+                    }
+
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                System.out.println("That didn't work!");
+            }
+
+        }) {
+            @Override
+            public String getBodyContentType() {
+                return "application/json";
+            }
+
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                Map<String, String> params = new HashMap<String, String>();
+                params.put("Content-Type", " application/json");
+                return params;
+            }
+        };
+        // Add the request to the RequestQueue.
+        queue.add(stringRequest);
+    }
+
+    public void shareRoom(View v, String roomName, String roomOwner, String shareWith) {
+        //String massageToSend = "test";
+        //final String message = massageToSend.getText().toString();
+        //massageToSend.setText("");
+        //System.out.println("Message: " + message);
+
+        RequestQueue queue = Volley.newRequestQueue(v.getContext());
+        String url = "http://158.38.193.60:8080/Shareable-Shopping-List-REST/api/rooms/addRoom?roomName=" + roomName + "&roomOwner=" + roomOwner;
+        // Request a string response from the provided URL.
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        // Display the first 500 characters of the response string.
+                        //System.out.println("Response is: "+ response /*response.substring(0,500)*/);
+                    }
+
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                System.out.println("That didn't work!");
+            }
+
+        }) {
+            @Override
+            public String getBodyContentType() {
+                return "application/json";
+            }
+
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                Map<String, String> params = new HashMap<String, String>();
+                params.put("Content-Type", " application/json");
+                return params;
+            }
+        };
+        // Add the request to the RequestQueue.
+        queue.add(stringRequest);
     }
 
 }
