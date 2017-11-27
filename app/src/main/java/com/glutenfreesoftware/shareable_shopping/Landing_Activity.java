56a3881fa29df17095a0394690d73b59fcfbc47c
oysteinbarstadLaptop <oebars@gmail.com>
2017-11-27 15:54:15 +0100

    Removed floating action buitton

--- app/src/main/java/com/glutenfreesoftware/shareable_shopping/Landing_Activity.java
+++ app/src/main/java/com/glutenfreesoftware/shareable_shopping/Landing_Activity.java
@@ -36,14 +36,6 @@ public class Landing_Activity extends AppCompatActivity
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                addList();
-            }
-        });
-
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
